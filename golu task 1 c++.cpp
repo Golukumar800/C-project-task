0cd3dd64c0bcd574d0ffdd395f9769ce24acaa2e
Golu Kumar <guptagolu8092@gmail.com>
2026-07-05 14:40:23 +0530

    Add files via upload

---
+++ golu task 1 c++.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    float totalCredits = 0, totalGradePoints = 0;
+
+    cout << "========== CGPA Calculator ==========\n";
+    cout << "Enter the number of courses: ";
+    cin >> n;
+
+    string courseName[n];
+    float grade[n], credit[n];
+
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << "\nCourse " << i + 1 << endl;
+
+        cout << "Enter Course Name: ";
+        cin >> courseName[i];
+
+        cout << "Enter Grade Point (0 - 10): ";
+        cin >> grade[i];
+
+        cout << "Enter Credit Hours: ";
+        cin >> credit[i];
+
+        totalCredits += credit[i];
+        totalGradePoints += grade[i] * credit[i];
+    }
+
+    float cgpa = totalGradePoints / totalCredits;
+
+
+    cout << "\n========== RESULT ==========\n";
+    cout << left << setw(20) << "Course"
+         << setw(15) << "Grade"
+         << setw(15) << "Credits" << endl;
+
+    cout << "--------------------------------------------\n";
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << left << setw(20) << courseName[i]
+             << setw(15) << grade[i]
+             << setw(15) << credit[i] << endl;
+    }
+
+    cout << "--------------------------------------------\n";
+    cout << "Total Credits      : " << totalCredits << endl;
+    cout << "Total Grade Points : " << totalGradePoints << endl;
+    cout << fixed << setprecision(2);
+    cout << "Semester GPA       : " << cgpa << endl;
+    cout << "Overall CGPA       : " << cgpa << endl;
+
+    return 0;
+}
